0332aa6d094bd3e5ceb52a40bf12fa7cc8564ac6
parames4631 <sr.parames_st@tni.ac.th>
2021-12-18 09:41:30 +0700

    React-router

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import NavBar from "./components/NavBar";
 
 import HomePage from "./pages/HomePage";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <>
       <div>
+        <Router>
         <NavBar />
-        <HomePage />
+        <Switch>
+          <Route exact path='/'><HomePage /></Route>
+          <Route path='/about'><AboutPage /></Route>
+        </Switch>
         <Footer />
-        
+        </Router>
         
       </div>
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Navbar , Nav , NavDropdown , Form, FormControl , Button } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+
 const NavBar = () => {
     return (
         <div>
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+                <NavLink className="navbar-brand" to="/" exact>React-Bootstrap</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
+                        {/* <Nav.Link href="#home">Home</Nav.Link> */}
+                        <NavLink className="nav-link" activeClassName="active" to="/" exact>Home</NavLink>
+                        <NavLink className="nav-link" activeClassName="active" to="/about">About</NavLink> 
                         <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
